475f67eb912b6dfb021d247d5227455914195851
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-08-14 15:31:46 +0330

    fix code style

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
     /**
      * Get the attributes that should be cast.
      *test
+     *
      * @return array<string, string>
      */
     protected function casts(): array
